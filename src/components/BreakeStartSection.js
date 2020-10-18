6ef9ee4a2a69864c9ad69b9081aacf2683e3f992
KolodziejczykTomasz <tomaszkolodziejczyk@wp.pl>
2020-10-18 19:15:16 +0200

    Correted style in AboutView.

--- src/components/BreakeStartSection.js
+++ src/components/BreakeStartSection.js
@@ -5,21 +5,14 @@ import 'components/BreakeStartSection.css';
 const BreakeStartSection = ({ children }) => (
   <div id="wrapperBreakeLine">
     <div id="wrapperItemLeft">
-      <div id="breakeLine">
-        <hr style={{ borderTop: '1.5px solid #1680BC' }} />
-      </div>
+      <hr style={{ borderTop: '1.5px solid #1680BC' }} />
     </div>
     <div id="wrapperItemCentral">
       <div id="wrapperText">{children}</div>
     </div>
     <div id="wrapperItemRight">
-      <div id="breakeLine">
-        <hr style={{ borderTop: '1.5px solid #1680BC' }} />
-      </div>
+      <hr style={{ borderTop: '1.5px solid #1680BC' }} />
     </div>
   </div>
 );
 export default BreakeStartSection;
-
-
-
