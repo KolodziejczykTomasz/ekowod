79eaaef930d5a5325b02639ae706e3c897240081
KolodziejczykTomasz <tomaszkolodziejczyk@wp.pl>
2020-10-30 21:10:08 +0100

    Added gallery in MainAbout.

---
+++ src/data/gallery.js
@@ -0,0 +1,21 @@
+import Pic1 from 'assets/images/gallery/building1.jpg';
+import Pic2 from 'assets/images/gallery/building2.jpg';
+import Pic3 from 'assets/images/gallery/building3.jpg';
+import Pic4 from 'assets/images/gallery/building4.jpg';
+import Pic5 from 'assets/images/gallery/building5.jpg';
+import Pic6 from 'assets/images/gallery/building6.jpg';
+import Pic7 from 'assets/images/gallery/building7.jpg';
+import Pic8 from 'assets/images/gallery/building8.jpg';
+
+export default [ 
+    
+      { id: 1, url: Pic1 },
+      { id: 2, url: Pic2 },
+      { id: 3, url: Pic3 },
+      { id: 4, url: Pic4 },
+      { id: 5, url: Pic5 },
+      { id: 6, url: Pic6 },
+      { id: 7, url: Pic7 },
+      { id: 8, url: Pic8 },
+   
+];
